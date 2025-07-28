ce7ca48c5c77ebb4bf03a3b7655c1913a3c37144
mo <saadshahidqavi2003@gmail.com>
2025-07-29 00:38:44 +0100

    footer texture url fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import githubLogo from "./../assets/logo-github.png";
 
 function Footer() {
     return (
-      <footer className="flex-col flex gap-4 items-center justify-end h-32 p-4 bg-accent bg-[url(stripe-texture.png)] bg-repeat bg-[length:50px] text-white text-sm">
+      <footer className="flex-col flex gap-4 items-center justify-end h-32 p-4 bg-accent bg-[url(/stripe-texture.png)] bg-repeat bg-[length:50px] text-white text-sm">
         <a className="flex flex-col place-content-center text-center" href="http://www.github.com/MoShahid23/" target="_blank">
           <img className="block w-30" src={githubLogo} alt="" />
         </a>

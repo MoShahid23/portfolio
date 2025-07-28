85076e0035038181bd3f5b00c9529a429b653cc7
mo <saadshahidqavi2003@gmail.com>
2025-07-28 17:10:26 +0100

    fixed image url

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ function Home() {
             id="Home"
             className="relative min-h-[calc(100vh-4rem)] flex justify-center items-center p-4 text-left bg-primary overflow-hidden"
         >
-            <div className="animate-fade-in-slow absolute inset-0 bg-[url(old-wall.png)] bg-repeat bg-[length:300px] pointer-events-none z-0" />
+            <div className="animate-fade-in-slow absolute inset-0 bg-[url(/old-wall.png)] bg-repeat bg-[length:300px] pointer-events-none z-0" />
             <div className="absolute inset-0  bg-primary opacity-10 pointer-events-none z-0" />
             <div className="relative z-10 fade flex flex-col items-start w-max mx-auto animate-slide">
               <div className="text-5xl font-bold leading-relaxed w-1/2 place-self-center">

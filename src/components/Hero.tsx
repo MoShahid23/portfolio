7d0644231a362ec9d932a40855629794e4de862f
mo <saadshahidqavi2003@gmail.com>
2025-07-28 18:29:57 +0100

    added favicon

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,11 +6,11 @@ function Home() {
         >
             <div className="animate-fade-in-slow absolute inset-0 bg-[url(/old-wall.png)] bg-repeat bg-[length:300px] pointer-events-none z-0" />
             <div className="absolute inset-0  bg-primary opacity-10 pointer-events-none z-0" />
-            <div className="relative z-10 fade flex flex-col items-start w-max mx-auto animate-slide">
-              <div className="text-5xl font-bold leading-relaxed w-1/2 place-self-center">
+            <div className="relative z-10 fade flex flex-col items-start w-1/1 mx-auto animate-slide">
+              <div className="text-5xl font-bold leading-relaxed place-self-center">
                   Hi, I'm Mo.
               </div>
-              <div className="text-4xl font-light w-1/2 place-self-center">
+              <div className="text-4xl font-light place-self-center">
                   I'm a full-stack developer
               </div>
             </div>
